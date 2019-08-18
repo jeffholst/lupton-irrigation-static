0f632a534d0c24a6bc41320a5cef4cda3e4b9863
Jeff Holst <jeffholst@gmail.com>
2019-08-18 17:43:35 -0500

    Pics and scrolling

--- js/init.js
+++ js/init.js
@@ -5,4 +5,17 @@
     $('.parallax').parallax();
 
   }); // end of document ready
+
+  $('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
 })(jQuery); // end of jQuery name space
